92e3c2b0fa30d0579326af413a6a69e02c1f63ad
Abraham galindo <abraham.garo@gmail.com>
2025-06-08 12:58:48 -0600

    chore: setup monorepo

---
+++ packages/component-library/src/Button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Button: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return <button style={{
+    padding: "0.5rem 1rem",
+    backgroundColor: "#007bff",
+    // color: "#fff"
+    color: "red",
+  }}>
+    {children}
+  </button>;
+};
